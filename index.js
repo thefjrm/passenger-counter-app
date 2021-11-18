3d90a31054f931c392cbb239154c4311ae45bdbe
thefjrm <felipemartinsplayernet@gmail.com>
2021-11-18 18:16:30 -0300

    Create Save and Increment Functions

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 let count = 0;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-  count = count + 1;
-  document.getElementById("count-el").innerHTML = count;
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = " " + count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
 }
